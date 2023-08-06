f8b5fee6c4e6f63d33477e6a46a677c4e4a27dd2
manish77s <69430012+manish77s@users.noreply.github.com>
2023-08-07 02:19:17 +0530

    Update StudentCompo.js

--- src/StudentCompo.js
+++ src/StudentCompo.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 function Student(){
-    const [data , strdata]= useState(
+    const [data]= useState(
         [
             {
                 Name : 'hero',
